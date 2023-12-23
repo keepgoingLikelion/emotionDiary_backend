912211fe50869892edcfec040c8e01e3eeb87112
GamJaDo <103554906+GamJaDo@users.noreply.github.com>
2023-12-23 21:05:53 +0900

    Add files via upload

---
+++ emotionDiary_backend/src/main/java/com/keepgoingLikeline/emotionDiary_backend/EmotionDiaryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.keepgoingLikeline.emotionDiary_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionDiaryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionDiaryBackendApplication.class, args);
+	}
+
+}
---
+++ emotionDiary_backend/src/test/java/com/keepgoingLikeline/emotionDiary_backend/EmotionDiaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keepgoingLikeline.emotionDiary_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionDiaryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
