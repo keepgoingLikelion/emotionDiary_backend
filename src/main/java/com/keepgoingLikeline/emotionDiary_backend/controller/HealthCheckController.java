1a68239e4722370877b8b0fe5d880dc540f095e5
GamJaDo <kim0615157@gmail.com>
2024-02-17 16:49:10 +0900

    address -> serverAddress 수정

--- src/main/java/com/keepgoingLikeline/emotionDiary_backend/controller/HealthCheckController.java
+++ src/main/java/com/keepgoingLikeline/emotionDiary_backend/controller/HealthCheckController.java
@@ -15,7 +15,7 @@ public class HealthCheckController {
 	private String env;
 	@Value("${server.port}")
 	private String serverPort;
-	@Value("${server.address}")
+	@Value("${server.serverAddress}")
 	private String serverAddress;
 	@Value("${serverName}")
 	private String serverName;
